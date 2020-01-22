40f1771d95a0a4150d5aa715edc9c8809a4ce9de
formulahunter <gayden.hunter@gmail.com>
2020-01-22 00:28:15 -0500

    Define window.game for debugging

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import SlitherLinkGame from './SlitherLinkGame.js';
 
 // function main(): void {
-    // @ts-ignore
     let canvas: HTMLElement | null = document.getElementById('game');
     if(canvas === null || !(canvas instanceof HTMLCanvasElement)) {
         throw new Error('missing or invalid canvas element');
     }
 
-    new SlitherLinkGame(23, canvas);
+    // @ts-ignore
+    window.game = new SlitherLinkGame(23, canvas);
 // }
