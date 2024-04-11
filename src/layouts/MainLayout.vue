4d1270aeb10ab24e23a92b0cbb96ad7344b4ed11
Hunter Gayden <gayden.hunter@gmail.com>
2024-04-10 23:58:24 -0400

    Consolidate input "pages" & refine layout
    Various groups of inputs (board settings, appearance, solution-finding) will be implemented in distinct components to be presented alongside the SVG document

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,51 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import NavLink, { NavLinkProps } from 'components/NavLink.vue';
 
 defineOptions({
   name: 'MainLayout'
 });
 
-const linksList: NavLinkProps[] = [
-  {
-    title: 'Generate',
-    caption: 'Generate random boards & adjust game parameters',
-    icon: 'tune', //  tune, edit, refresh, sync
-    link: '/generate'
-  },
-  {
-    title: 'Solve',
-    caption: 'Inspect game/board constraints & find solution',
-    icon: 'o_extension', //  extension, troubleshoot
-    link: '/solve'
-  },
-  {
-    title: 'Appearance',
-    caption: 'Visual style settings',
-    icon: 'o_shape_line', // shape-line, polyline, format-shapes, hexagon, style
-    link: '/appearance'
-  },
-];
-
-const leftDrawerOpen = ref(false);
-
-function toggleLeftDrawer () {
-  leftDrawerOpen.value = !leftDrawerOpen.value;
-}
 </script>
 
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
 
         <q-toolbar-title>
           SlitherLink
@@ -54,27 +18,13 @@ function toggleLeftDrawer () {
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Navigation
-        </q-item-label>
-
-        <NavLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
+
+<style lang="sass">
+.q-page-container
+  padding: 0
+</style>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,10 +5,7 @@ const routes: RouteRecordRaw[] = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: 'appearance', component: () => import('pages/AppearancePage.vue') },
-      { path: 'solve', component: () => import('pages/SolvePage.vue') },
-      { path: 'generate', component: () => import('pages/GeneratePage.vue') },
-      { path: '', redirect: 'generate' },
+      { path: '', component: () => import('pages/MainPage.vue') },
     ],
   },
 
